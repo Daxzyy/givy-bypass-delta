9f5a9607cd1d1360884d7d125dd1bf8459b9e894
Daxzyy <122425004+Daxzyy@users.noreply.github.com>
2026-03-20 09:07:15 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,2 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  env: {
-    // GIVY_SECRET di-inject ke frontend saat build dari Vercel Env Vars
-    NEXT_PUBLIC_GIVY_SECRET: process.env.GIVY_SECRET || '',
-  },
-};
+const nextConfig = {};
 module.exports = nextConfig;
